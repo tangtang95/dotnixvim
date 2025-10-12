790bba8eec335e5057f0c4bbfa0a7e4a542eadb4
Tangtang Zhou <tangtang2995@gmail.com>
2025-10-12 12:00:27 +0200

    chore: add grug far for search and replace

--- config/plugins/editor.nix
+++ config/plugins/editor.nix
@@ -1,5 +1,25 @@
 {
   plugins = {
     nvim-autopairs.enable = true;
+    grug-far = {
+      enable = true;
+      settings = {
+        headerMaxWidth = 80;
+      };
+    };
   };
+
+  keymaps = [
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "<leader>sr";
+      action = "<cmd>GrugFar<cr>";
+      options = {
+        desc = "Search and Replace";
+      };
+    }
+  ];
 }

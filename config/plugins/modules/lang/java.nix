0444e572863a004f627c8e6571dc436a5e76fda5
Tangtang Zhou <tangtang2995@gmail.com>
2025-11-02 21:36:22 +0100

    chore: add lombok to jdtls

--- config/plugins/modules/lang/java.nix
+++ config/plugins/modules/lang/java.nix
@@ -12,9 +12,17 @@
 
   plugins.jdtls = {
     enable = true;
+    luaConfig.pre = ''
+      local cacheDir = vim.fn.stdpath('cache') .. '/nvim-jdtls'
+    '';
     settings = {
       cmd = [
         "${pkgs.jdt-language-server}/bin/jdtls"
+        "--jvm-arg=-javaagent:${pkgs.lombok}/share/java/lombok.jar"
+        "-configuration"
+        (lib.nixvim.mkRaw "cacheDir .. '/' .. 'jdtls-config'")
+        "-data"
+        (lib.nixvim.mkRaw "cacheDir .. '/' .. vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')")
       ];
       settings = {
         java = {
